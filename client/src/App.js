2e47ff5cd0613039fa0621e6603d471673dca03a
Ericjr688 <ericasare688@gmail.com>
2024-06-07 21:20:46 -0400

    Set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage () {
+  return ( 
+    <div className="Home">
+      <div>Home Page!</div>
+      <p>text random</p>
+    </div>
+   );
+}
+
+export default HomePage;
