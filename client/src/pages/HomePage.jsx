33ffec5b79b9d47047b12f01ebba304d973c50b0
Ericjr688 <ericasare688@gmail.com>
2024-06-11 03:27:04 -0400

    completed latest improvements skeleton

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,9 +5,9 @@ function HomePage () {
   const popularNovels = [
     {
       id: 1,
-      title: "The Legendary Mechanic",
+      title: "The Legendary Mechanic", 
       img: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1557871302l/45807334.jpg",
-      chapters: 1200,
+      chapters: 1200, // should chapters be displayed? we may not have all the chapters, so it what kind of chapter info do we dislay. might as well not display then
       score: 4.9,
     },
     {
@@ -19,36 +19,63 @@ function HomePage () {
     },
     {
       id: 3,
-      title: "The Legendary Mechanic",
-      img: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1557871302l/45807334.jpg",
+      title: "Noble Emblem",
+      img: "https://cdn.novelupdates.com/images/2016/12/180-1.jpg",
       chapters: 1200,
-      score: 4.9,
+      score: 4.8,
     },
     {
       id: 4,
-      title: "Eye of Evolution",
-      img: "https://www.mtlnovel.net/2020/03/The-Eye-of-Evolution.jpg.webp",
-      chapters: 800,
+      title: "Transdimensional Marketing",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH_lT1szOlA21FFYx2HFfedD1t_rB4Busokg&s",
+      chapters: 400,
       score: 4.5,
     },
     {
       id: 5,
-      title: "The Legendary Mechanic",
-      img: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1557871302l/45807334.jpg",
-      chapters: 1200,
-      score: 4.9,
+      title: "Sylver Seeker",
+      img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1620489522i/57549500.jpg",
+      chapters: 300,
+      score: 4.2,
     },
     {
       id: 6,
-      title: "Eye of Evolution",
-      img: "https://www.mtlnovel.net/2020/03/The-Eye-of-Evolution.jpg.webp",
+      title: "Absolute Great Teacher",
+      img: "https://cdn.novelupdates.com/images/2020/10/absolutegreatteacher3.jpg",
       chapters: 800,
       score: 4.5,
     }
   ]
 
+  const latestImprovements = [
+    {
+      id: 1,
+      novelId: 1, 
+      title: "The Legendary Mechanic",
+      chapter: 819,
+      contributor: "dao ancestor",
+      date: "2024/06/11", 
+    },
+    {
+      id: 2,
+      novelId: 3, 
+      title: "Noble Emblem",
+      chapter: 598,
+      contributor: "dao ancestor",
+      date: "2024/06/10", 
+    },
+    {
+      id: 3,
+      novelId: 5, 
+      title: "Sylver Seeker",
+      chapter: 267,
+      contributor: "minced watermelon",
+      date: "2024/06/10", 
+    }
+  ]
+
   // include function to hyphenate and lowercase novel name so that we use that to go to novel page rather than id (placeholder). remember to change in router as well
-  // removed author and desc and latest chapter and tags and status
+  // removed author and desc and latest chapter and tags and status in popular novels
   return ( 
     <div className="home page-wrapper">
       <div className="banner"> Banner</div>
@@ -74,10 +101,36 @@ function HomePage () {
           }
         </div>
       </div>
-      <div className="latest-releases section">
+
+      <div className="latest-improvements section">
         <h2 className="section-header">
-          Latest Releases
+          Latest Improvements
         </h2>
+        <div className="update-table">
+          <table cellPadding={0} cellSpacing={0}>
+            <thead>
+              <tr>
+                <th className="name">Novel Name</th>
+                <th className="chapter">Chapter</th>
+                <th className="user">Contributor</th>
+                <th className="date">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {latestImprovements.map(latestImprovement => (
+                <tr key={latestImprovement.id}>
+                  <Link className= "link" to={`/series/${latestImprovement.novelId}`}><td className="name">{latestImprovement.title}</td></Link>
+                  <td className="chapter">Chapter {latestImprovement.chapter}</td> {/* create page for chapter and insert into router. how will the url of chapters be displayed*/}
+                  <td className="user">{latestImprovement.contributor}</td>
+                  <td className="date">{latestImprovement.date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+
+          
+        </div>
       </div>
       <div className="recommendations section">
         <h2 className="section-header">
